d6b127a36e431d2bcf561f638d935aac6525377c
rezadaffa <rezadaffa375@gmail.com>
2018-02-19 07:41:02 +0700

    He3x

--- application/views/adminlte.php
+++ application/views/adminlte.php
@@ -335,9 +335,7 @@
         <li class="header">MAIN NAVIGATION</li>
         <li class="active treeview">
           <a href="#">
-            <i class="fa fa-dashboard"></i> <span>Dashboard</span>
-            <span class="pull-right-container">
-              <i class="fa fa-angle-left pull-right"></i>
+            
             </span>
           </a>
           <ul class="treeview-menu">
